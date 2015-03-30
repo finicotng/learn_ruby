4fdf2dd4ecf1f9633c1653bb73d3c8b555c575a1
finico <finico.a@gmail.com>
2015-03-31 00:05:24 +0300

    operators

---
+++ 03_operators.rb
@@ -0,0 +1,19 @@
+name = 'John'
+puts name
+puts 1 + 2
+a = 8 - 6
+puts a
+puts a * 4
+puts 10 / a
+
+puts 1 == 3
+puts 1 < 2
+puts 5 > 4
+puts 2 <= 2
+puts 3 >= 3
+
+puts (2 + 4) * 4
+puts 2 > 1 && 3 < 4
+puts name == 'John' && a == 1 + 1
+
+puts name == 'Finico' || name == 'John'
